437ebd3442a66f62bc9a758ce1db31aba4cfe011
TheAnosmic <theanosmic@gmail.com>
2026-03-30 16:27:11 +0300

    add types

--- Assets/Scripts/CombatEnums.cs
+++ Assets/Scripts/CombatEnums.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
 
 // DO NOT PUT A CLASS HERE. These must be global.
-public enum DieType { Attack, Defense }
-public enum FaceRarity { Common, Rare, Legendary }
+public enum DieType
+{
+    Shadow,
+    Defense,
+    Fire,
+    Ice,
+    Nature,
+}
+
+public enum FaceRarity
+{
+    Common,
+    Rare,
+    Legendary
+}
 // Note: CombatState was moved to its own file previously
