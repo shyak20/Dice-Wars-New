98a02eae4389b754d8287bd93c3c8a7184ccd4e2
shyak2020 <shay.s@sneaky-panda.com>
2026-04-30 12:28:36 +0300

    New Main Menu

--- Assets/Scripts/Shop/UIShopOfferCardView.cs
+++ Assets/Scripts/Shop/UIShopOfferCardView.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIShopOfferCardView : MonoBehaviour
@@ -13,6 +14,8 @@ public class UIShopOfferCardView : MonoBehaviour
     [SerializeField] Color unaffordablePriceColor = new(1f, 0.35f, 0.35f);
     [SerializeField] Button buyButton;
     [SerializeField] GameObject soldStamp;
+    [Tooltip("Shown while the pointer hovers the buy button (requires raycast target on the button Image).")]
+    [SerializeField] GameObject hoverOnBuyButton;
 
     int _price;
     bool _sold;
@@ -21,7 +24,38 @@ public class UIShopOfferCardView : MonoBehaviour
     void Awake()
     {
         if (buyButton != null)
+        {
             buyButton.onClick.AddListener(() => _onBuy?.Invoke());
+            SetupBuyButtonHover();
+        }
+
+        SetHoverVisible(false);
+    }
+
+    void OnDisable() => SetHoverVisible(false);
+
+    void SetupBuyButtonHover()
+    {
+        if (hoverOnBuyButton == null)
+            return;
+
+        var go = buyButton.gameObject;
+        var et = go.GetComponent<EventTrigger>() ?? go.AddComponent<EventTrigger>();
+        et.triggers.RemoveAll(e => e.eventID == EventTriggerType.PointerEnter || e.eventID == EventTriggerType.PointerExit);
+
+        var enter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+        enter.callback.AddListener(_ => SetHoverVisible(true));
+        et.triggers.Add(enter);
+
+        var exit = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+        exit.callback.AddListener(_ => SetHoverVisible(false));
+        et.triggers.Add(exit);
+    }
+
+    void SetHoverVisible(bool visible)
+    {
+        if (hoverOnBuyButton != null)
+            hoverOnBuyButton.SetActive(visible);
     }
 
     public void Bind(string title, string desc, Sprite icon, int price, bool canAfford, bool sold, Action onBuy)
@@ -42,6 +76,7 @@ public class UIShopOfferCardView : MonoBehaviour
         if (soldStamp != null) soldStamp.SetActive(sold);
         if (buyButton != null) buyButton.interactable = !sold && canAfford;
         if (priceText != null) priceText.color = sold || canAfford ? affordablePriceColor : unaffordablePriceColor;
+        SetHoverVisible(false);
     }
 
     public void RefreshAffordability()
